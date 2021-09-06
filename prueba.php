88d45aa31140655ccb7c6b0ad3317de74f76ebf1
potzmevi <68794813+potzmevi@users.noreply.github.com>
2021-09-05 23:35:10 -0600

    Hola mundo
    Agregada animacion en js

---
+++ prueba.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>TS1 Hola Mundo</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+
+<link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<br />  <br />
+	 <div class="foo"> Hola mundo </div>
+	 <div class="foo">TS1 #12</div> 
+  
+  <br />
+  <br />
+
+<?php
+  echo "Juan Pablo Meza Vielman 201930268";
+?>
+  <br />
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
+
+</body>
+</html>
+
